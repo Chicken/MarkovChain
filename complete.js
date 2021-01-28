bdf53943bd2a3ad6f80f3c1d8627290d8764ac49
Antti <antti@antti.codes>
2021-01-28 17:49:20 +0200

    complete script

---
+++ complete.js
@@ -0,0 +1,33 @@
+let [ source = "./data/model.json", amount, ...comp ] = process.argv.slice(2);
+
+const model = require(`./${source}`);
+
+function wRandom(arr) {
+    let wArr = [];
+    for(let item of arr) for(let i = 0; i < item.weight; i++) wArr.push(item.value); 
+    return wArr[Math.floor(Math.random() * wArr.length)];
+}
+
+let start = Date.now();
+
+let first = comp.pop();
+let sentences = [];
+
+for(let i = 0; i < amount; i++) {
+    let sentence = [];
+
+    let next = first;
+    while (next != null) {
+        sentence.push(next);
+        next = wRandom(model[model.findIndex(n => n.value === next)].next);
+    }
+
+    let text = comp.concat(sentence).join(" ");
+    if(sentences.includes(text)) {
+        amount++;
+    } else {
+        sentences.push(text);
+    }
+}
+
+console.log(sentences.map((v,i) => `${i+1}. ${v}`).join("\n") + `\nGenerated in ${Date.now() - start}ms.`);
